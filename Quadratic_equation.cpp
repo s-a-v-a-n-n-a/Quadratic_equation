76195baabb96bc058ac275bc70de7818c31ff357
s-a-v-a-n-n-a <70327418+s-a-v-a-n-n-a@users.noreply.github.com>
2020-08-27 15:50:18 +0300

    Add files via upload

---
+++ Quadratic_equation.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <math.h>
+
+#define max(A, B) ((A) > (B) ? (A) : (B))
+#define min(A, B) ((A) < (B) ? (A) : (B))
+
+double discriminant(double a, double b, double c)
+{
+    return b*b - 4*a*c;
+}
+
+void const_eq(double c)
+{
+    if (c == 0)
+        printf("All numbers are roots");
+    else printf("No roots");
+    return ;
+}
+
+void line_eq(double b, double c)
+{
+    printf("1 root, x = %lg", -c/b);
+    return ;
+}
+
+void sq_eq(double a, double b, double c)
+{
+    double discrim = discriminant(a, b, c);
+    if (discrim < 0)
+        printf("No roots");
+    else if(discrim > 0)
+    {
+        double x1 = (-b + sqrt(discrim))/(2*a), x2 = (-b - sqrt(discrim))/(2*a);
+        printf("2 roots, x1 = %lg, x2 = %lg", min(x1, x2), max(x1, x2));
+    }
+    else printf("1 root, x = %lg", -b/(2*a));
+    return ;
+}
+
+int main()
+{
+    double a = 0, b = 0, c = 0;
+
+    scanf("%lg %lg %lg", &a, &b, &c);
+
+    if (a == 0 && b == 0)
+            const_eq(c);
+    else if (a == 0)
+            line_eq(b, c);
+    else sq_eq(a, b, c);
+    return 0;
+}
