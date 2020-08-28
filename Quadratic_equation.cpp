308f196b5a940cc62b11149b6a4b944d8e02ff88
s-a-v-a-n-n-a <70327418+s-a-v-a-n-n-a@users.noreply.github.com>
2020-08-29 01:43:21 +0300

    Update Quadratic_equation.cpp

--- Quadratic_equation.cpp
+++ Quadratic_equation.cpp
@@ -1,47 +1,86 @@
 #include <stdio.h>
+#include <assert.h>
 #include <math.h>
 
-#define max(A, B) ((A) > (B) ? (A) : (B))
-#define min(A, B) ((A) < (B) ? (A) : (B))
+#define INF -1
 
-double discriminant(double a, double b, double c)
-{
-    return b*b - 4*a*c;
-}
+//-------------------------------------------------------
+// Swaps to numbers
+//
+// Parameters:
+//
+// a1   address of the first number
+// a2   address of the second number
+//-------------------------------------------------------
+void swap(double *a1, double *a2);
 
-//функция для работы с константными выражениями
-void const_eq(double c)
-{
-    if (c == 0)
-        printf("All numbers are roots");
-    else 
-        printf("No roots");
-}
+//-------------------------------------------------------
+// Finds a discriminant
+//
+// Parameters:
+//
+// a    the first  coefficient
+// b    the second coefficient
+// c    the third  coefficient
+//
+// Returns:
+//
+// the discriminant
+//-------------------------------------------------------
+double discriminant(double a, double b, double c) ;
 
-//функция для работы с линейными уравнениями
-void line_eq(double b, double c)
-{
-    printf("1 root, x = %lg", -c/b);
-}
 
-//функция для работы с квадратными уравнениями
-void sq_eq(double a, double b, double c)
-{
-    double discrim = discriminant(a, b, c);
-    if (discrim < 0)
-        printf("No roots");
-    else if(discrim > 0)
-    {
-        double x1 = (-b + sqrt(discrim))/(2*a);
-        double x2 = (-b - sqrt(discrim))/(2*a);
-        printf("2 roots, x1 = %lg, x2 = %lg", min(x1, x2), max(x1, x2));
-    }
-    else 
-    {
-        b = (b == 0) ? -b : b; //чтобы не было -0
-        printf("1 root, x = %lg", -b/(2*a));
-    }
-}
+//-------------------------------------------------------
+// Solves a const equation c = 0
+//
+// Parameters:
+//
+// c    the third coefficient
+//
+// Returns:
+//
+// flag     the number of roots
+//-------------------------------------------------------
+int const_eq(double c) ;
+
+//-------------------------------------------------------
+// Solves a line equation bx + c = 0 when b != 0
+//
+// Parameters:
+//
+// b    the second coefficient
+// c    the third  coefficient
+//
+// Returns:
+//
+// flag     the number of roots
+// x1       the root
+//-------------------------------------------------------
+int line_eq(double b, double c, double *x1);
+
+
+//-------------------------------------------------------
+// Solves a square equation ax^2 + bx + c = 0 when a != 0
+//
+// Parameters:
+//
+// a    the first  coefficient
+// b    the second coefficient
+// c    the third  coefficient
+//
+// Returns:
+//
+// flag     the number of roots
+// x1       the first root
+// x2       the second root
+//-------------------------------------------------------
+int sq_eq(double a, double b, double c, double *x1, double *x2);
+
+void unit_tests_for_consteq();
+
+void unit_tests_for_line_eq();
+
+void unit_tests_for_squareq();
 
 int main()
 {
@@ -49,13 +88,177 @@ int main()
     double b = 0;
     double c = 0;
 
+    int kol = 0;
+
+    double x1 = 0;
+    double x2 = 0;
+
+    unit_tests_for_consteq();
+    unit_tests_for_line_eq();
+    unit_tests_for_squareq();
+
     scanf("%lg %lg %lg", &a, &b, &c);
 
     if (a == 0 && b == 0)
-            const_eq(c);
+    {
+            kol = const_eq(c);
+            if (kol == 0)
+                printf("All numbers are roots\n");
+            else
+                printf("No roots");
+    }
     else if (a == 0)
-            line_eq(b, c);
-    else 
-            sq_eq(a, b, c);
+    {
+            kol = line_eq(b, c, &x1);
+            if (kol == -2)
+                printf("This is not a line equation\n");
+            else
+                printf("1 root, x = %lg", x1);
+    }
+    else
+    {
+        kol = sq_eq(a, b, c, &x1, &x2);
+        if (kol == -2 )
+            printf("This is not a square equation\n");
+        else if (kol == 0)
+            printf("No roots\n");
+        else if (kol == 2)
+            printf("2 roots, x1 = %lg, x2 = %lg\n", x1, x2);
+        else
+            printf("1 root, x = %lg\n", x1);
+    }
     return 0;
+}
+
+void swap(double *a1, double *a2)
+{
+    double tmp = *a1;
+    *a1 = *a2;
+    *a2 = tmp;
+}
+
+double discriminant(double a, double b, double c)
+{
+    return b*b - 4*a*c;
+}
+
+int const_eq(double c)
+{
+    if (c == 0)
+    {
+        return INF;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int line_eq(double b, double c, double *x1)
+{
+    if (b == 0)
+    {
+        return -2;
+    }
+
+    *x1 = -c/b;
+    return 1;
+}
+
+int sq_eq(double a, double b, double c, double *x1, double *x2)
+{
+    if (a == 0)
+    {
+        return -2;
+    }
+
+    double discrim = discriminant(a, b, c);
+    if (discrim < 0)
+    {
+        return 0;
+    }
+    else if(discrim > 0)
+    {
+        *x1 = (-b + sqrt(discrim))/(2*a);
+        *x2 = (-b - sqrt(discrim))/(2*a);
+
+        if (*x1 > *x2)
+            swap(x1, x2);
+        return 2;
+    }
+    else
+    {
+        b = (b == 0) ? -b : b;
+        *x1 = -b/(2*a);
+        return 1;
+    }
+}
+
+void unit_tests_for_consteq()
+{
+    {
+        int res = const_eq(100);
+        assert(res ==  0);
+    }
+
+    {
+        int res = const_eq(0);
+        assert(res ==  INF);
+    }
+}
+
+void unit_tests_for_line_eq()
+{
+    {
+        double x1 = 0;
+
+        int res = line_eq(5, 2, &x1);
+        assert(res ==  1);
+        assert(x1 == -0.4);
+    }
+
+    {
+        double x1 = 0;
+
+        int res = line_eq(0, 9, &x1);
+        assert(res ==  -2);
+    }
+}
+
+void unit_tests_for_squareq()
+{
+    {
+        double x1 = 0;
+        double x2 = 0;
+
+        int res = sq_eq(0, 5, 2, &x1, &x2);
+        assert(res ==  -2);
+    }
+
+    {
+        double x1 = 0;
+        double x2 = 0;
+
+        int res = sq_eq(1, 4, 4, &x1, &x2);
+        assert(res ==  1);
+        assert(x1 == -2);
+    }
+
+    {
+        double x1 = 0;
+        double x2 = 0;
+
+        int res = sq_eq(1, 2, 5, &x1, &x2);
+        assert(res ==  0);
+    }
+
+    {
+        double x1 = 0;
+        double x2 = 0;
+
+        int res = sq_eq(1, -2, -3, &x1, &x2);
+        assert(res ==  2);
+        assert(x1 == -1);
+        assert(x2 == 3);
+    }
 }
